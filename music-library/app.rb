0076e2e16f25840ad364ab61296ed1616c6371e4
Verity <veritywong90@gmail.com>
2023-04-24 18:05:57 +0100

    test driving a repo class; demo

---
+++ music-library/app.rb
@@ -0,0 +1,9 @@
+require_relative 'lib/database_connection'
+
+DatabaseConnection.connect('music_library')
+
+result = DatabaseConnection.exec_params('SELECT * FROM artists;', [])
+
+result.each do |record|
+    p record
+end
---
+++ music-library/lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+
+    # Replace the attributes by your own columns.
+    attr_accessor :id, :name, :genre
+  end
+  
+  # The keyword attr_accessor is a special Ruby feature
+  # which allows us to set and get attributes on an object,
+  # here's an example:
+  #
+  # artist = artist.new
+  # artist.name = 'Radiohead'
+  # artist.name
