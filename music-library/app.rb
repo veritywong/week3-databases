fab7a63402a0f49a710125b87b5e9afb68508c4a
Verity <veritywong90@gmail.com>
2023-04-25 11:46:32 +0100

    successful run of app.rb

--- music-library/app.rb
+++ music-library/app.rb
@@ -1,10 +1,17 @@
 require_relative 'lib/database_connection'
 require_relative 'lib/artist_repository'
+require_relative 'lib/album_repository'
 
 DatabaseConnection.connect('music_library')
 
 artist_repository = ArtistRepository.new
 
-artist_repository.all.each do |artist|
-    p artist
+# artist_repository.all.each do |artist|
+#     p artist
+# end
+
+album_repository = AlbumRepository.new
+
+album_repository.all.each do |album|
+    p album
 end
