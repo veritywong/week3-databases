5d6cc6c096431ac9babad87ba54f5989f1ec5e35
Verity <veritywong90@gmail.com>
2023-04-25 12:27:25 +0100

    new project book store set-up

---
+++ book_store/app.rb
@@ -0,0 +1,9 @@
+require_relative 'lib/database_connection'
+
+DatabaseConnection.connect('book_store')
+
+result = DatabaseConnection.exec_params('SELECT * FROM books', [])
+
+result.each do |record|
+    p record
+end
