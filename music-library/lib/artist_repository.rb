e9a18e3a86aaae45ee1774a2b4b873b957a3cfef
Verity <veritywong90@gmail.com>
2023-04-25 10:18:47 +0100

    first red test for demo video

---
+++ music-library/lib/artist_repository.rb
@@ -0,0 +1,2 @@
+class ArtistRepository
+end
---
+++ music-library/spec/artist_repository_spec.rb
@@ -0,0 +1,26 @@
+require 'artist_repository'
+
+
+RSpec.describe ArtistRepository do
+
+    def reset_artists_table
+        seed_sql = File.read('spec/seeds_artists.sql')
+        connection = PG.connect({ host: '127.0.0.1', dbname: 'artists' })
+        connection.exec(seed_sql)
+    end
+      
+    describe ArtistRepository do
+        before(:each) do 
+          reset_artists_table
+        end
+    end
+
+    it "gets all artists" do
+        repo = ArtistRepository.new
+        artists = repo.all
+        expect(artists.length).to eq 2
+        expect(artists.first.id).to eq 1
+        expect(artists.find.name).to eq 'Pixies'
+
+    end
+end
